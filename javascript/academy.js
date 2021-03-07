6f06e6a51a02345ff7ba89ead7e56dc0f2d0b725
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2021-03-07 18:58:22 +0200

    update

---
+++ javascript/academy.js
@@ -0,0 +1,17 @@
+const tl = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#hero",
+		start: "top top",
+		end: "bottom top",
+		scrub: true
+	}
+});
+
+gsap.utils.toArray(".parallax").forEach(layer => {
+	const depth = layer.dataset.depth;
+	const movement = -(layer.offsetHeight * depth)
+	tl.to(layer, {y: movement, ease: "none"}, 0)
+});
+
+
+
